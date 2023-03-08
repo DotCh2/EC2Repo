373500fe498fee2464dbd8a01a4699a1bba0a745
DotCh2 <emilianochavezdelatorre@gmail.com>
2023-03-08 04:47:30 +0000

    Updates to App

--- app.js
+++ app.js
@@ -2,36 +2,37 @@
 
 // the above line will be important when we make this an executable service
 const http = require("http"); // import the http library from node
-const express = require('express'); // import the express library
+const express = require('express'); // import the express librar
+const bodyParser = require('body-parser')//This body parser is needed 
+//to help turn form data into objects.
 const app = express(); // create an instance of express
 const PORT = 3000; // the port we will listen on
 app.listen(PORT, () => console.log(`Listening on ${ PORT }`)); // start the server
 
-app.use(express.json()); // support json encoded bodies
+//This is middleware to parse the request body, essentially the from data
+app.use(bodyParser.urlencoded({ extended: true }));
 
 //This is just a test to see if the server is working
 const listOfThings = [
-	{ first:"John", last:"Doe", age:"22"},
-	{ first:"Jane", last:"Doe", age:"21"},
-	{ first:"John", last:"Smith", age:"23"}
-
 ] 
 
+
 //Creating a GET request to get all the things in the list of things
-app.get('/users', (req, res) => {
+app.get('/api', (req, res) => {
 	res.json(listOfThings);
 });
 
 //Making a POST request to add a new thing to the list of things
-app.post('/users', (req, res) => {
-	console.log(req.body);
-	listOfThings.push(req.body);
+app.post('/api', (req, res) => {
+	const {first, last, age} = req.body;
+	user = {first, last, age};
+	listOfThings.push(user)
 	res.json(listOfThings);
 });
 
 //Making a PUT request to update a thing in the list of things based on the first name 
 //of the user
-app.put('/users', (req, res) => {
+app.put('/api', (req, res) => {
 	console.log(req.body);
 	for (var i = 0; i < listOfThings.length; i++) {
 		if (listOfThings[i].fname == req.body.fname) {
@@ -42,14 +43,13 @@ app.put('/users', (req, res) => {
 });
 
 //Making a DELETE request to delete a thing in the list of things based on the first name
-app.delete('/users', (req, res) => {
+app.delete('/api', (req, res) => {
 	console.log(req.body);
 	for (var i = 0; i < listOfThings.length; i++) {
 		if (listOfThings[i].fname == req.body.fname) {
-			listOfThings.splice(i, 1);
+			listOfThings.splice(i, 1)
 		}
 	}
 
 	res.json(listOfThings);
-});
-
+});;
